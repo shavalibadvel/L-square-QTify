7209f435a18d8f6a1f8c8186b3951d735b158f02
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-02-24 21:14:21 +0530

    Add assets folder and logo

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./Search.module.css";
-import { ReactComponent as SearchIcon } from "../../assets/search-icon.png"; 
+// 1. Regular import for PNG
+import searchIcon from "../../assets/search-icon.svg"; 
 
 const Search = ({ placeholder }) => {
   return (
     <form className={styles.wrapper}>
       <input className={styles.search} placeholder={placeholder} />
       <button className={styles.searchButton}>
-        <SearchIcon />
+        {/* 2. Use an img tag instead of <SearchIcon /> */}
+        <img src={searchIcon} alt="search icon" width={20} />
       </button>
     </form>
   );
