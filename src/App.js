20b100f51efb6cac5c246359272021e23edd45aa
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-03-06 20:38:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
@@ -6,7 +6,7 @@ import AlbumPage from "./Pages/AlbumPage/AlbumPage";
 import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
 import { fetchTopAlbums, fetchNewAlbums, fetchSongs } from "./api/api";
 import styles from "./App.module.css";
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 
 function App() {
   const [allData, setAllData] = useState([]); 
